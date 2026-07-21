2b07d4ad1306a6857d4a20e7f9814aba31843c63
Nikita <nikitatsarenko7@gmail.com>
2026-07-21 15:16:39 +0300

    chore: add configs package

---
+++ internal/configs/config.go
@@ -0,0 +1,42 @@
+// Package configs provides a struct for managing .env variables
+package configs
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	*DBConfig
+}
+
+type DBConfig struct {
+	DSN string `env:"DSN"`
+}
+
+func LoadConfig() *Config {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatalf("godotenv error: %s", err.Error())
+	}
+
+	conf := &Config{}
+	errors := ParseConfig(conf)
+
+	if len(errors) > 0 {
+		printErrors(errors)
+	}
+
+	return conf
+}
+
+func printErrors(errs []error) {
+	var msg strings.Builder
+	msg.WriteString("config errors:\n")
+	for _, err := range errs {
+		fmt.Fprintf(&msg, "  - %s\n", err.Error())
+	}
+	log.Fatal(msg.String())
+}
